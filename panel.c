2cd5491cea9952794ab2296b9a539dfc16417375
Brett Gordon <beretta42@gmail.com>
2019-09-13 13:28:28 -0400

    panel: simplify

--- panel.c
+++ panel.c
@@ -12,15 +12,11 @@ void do_panel(widget *w, int ev) {
 
 
 void select_panel(widget *w, int sel) {
-    widget *n;
-    for (n = gp(w->child); n; n = gp(n->next)) {
-	if (sel-- == 0) {
-	    draw_back(w);
-	    do_event(w, EV_DRAW);
-	    n->flags &= ~FL_NODRAW;
-	    draw_all(n);
-	}
-	else
-	    n->flags |= FL_NODRAW;
+    if (w->d != sel) {
+	child_by_index(w, w->d)->flags |= FL_NODRAW;
+	child_by_index(w, sel)->flags &= ~FL_NODRAW;
+	draw_back(w);
+	draw_all(w);
+	w->d = sel;
     }
 }
